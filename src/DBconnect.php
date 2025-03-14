b6bc407142c8d4ed371482c1d657945d6acaa700
codeHysteria28 <graphicglobal67@gmail.com>
2025-03-14 21:06:41 +0000

    completed the read functionality

--- src/DBconnect.php
+++ src/DBconnect.php
@@ -1,5 +1,6 @@
 <?php
-require_once '../config.php'; //access the login values
+require_once __DIR__ . '/../config.php'; //access the login values
+
 try {
     $connection = new PDO($dsn, $username, $password, $options);
     echo 'DB connected';
