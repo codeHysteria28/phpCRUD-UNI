9b8f77a248891db4dc176b6a31330f1846cba59e
codeHysteria28 <graphicglobal67@gmail.com>
2025-03-14 20:42:22 +0000

    added functionality to save user and sanitize the input

--- create.php
+++ create.php
@@ -1,6 +1,37 @@
-<?php include "templates/header.php"; ?>
-
 <h2>Add a user</h2>
+<?php
+
+if(isset($_POST["submit"])){
+    require "common.php";
+
+    try{
+        require_once "src/DBconnect.php";
+
+        $new_user = array(
+            "firstname" => escape($_POST["firstname"]),
+            "lastname" => escape($_POST["lastname"]),
+            "email" => escape($_POST["email"]),
+            "age" => escape($_POST["age"]),
+            "location" => escape($_POST["location"])
+        );
+
+        $sql = sprintf( "INSERT INTO %s (%s) values (%s)", "users", implode(", ",
+            array_keys($new_user)), ":" . implode(", :", array_keys($new_user)) );
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+
+    }catch (PDOException $error){
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+
+include "templates/header.php";
+if(isset($_POST["submit"]) && $statement){
+    echo $new_user["firstname"] . 'successfully added';
+}
+?>
+
 <form method="post">
     <label for="firstname">First Name</label>
     <input type="text" name="firstname" id="firstname" required>
