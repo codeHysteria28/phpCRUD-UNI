6c73c79de978ebd87b7e217846e98a61f3695c9d
codeHysteria28 <graphicglobal67@gmail.com>
2025-03-14 20:08:48 +0000

    added footer,header and created pages for create and read functionality + some basic styles

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// Created by:
+// Branislav Buna
+// TU Dublin
+// B00158771
+
+include "templates/header.php";
+?>
+
+
+
+<ul>
+    <li>
+        <a href="create.php"><strong>Create</strong></a> - add a user
+    </li>
+    <li>
+        <a href="read.php"><strong>Read</strong></a> - find a user
+    </li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
